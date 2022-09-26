d453e465bb05db496b6b98f5629ed1bf44666353
alexvent10 <alessandrovent@protonmail.com>
2022-09-26 17:19:48 +0200

    minor

--- src/registrazione/registrazione.js
+++ src/registrazione/registrazione.js
@@ -1,20 +1,29 @@
-let form = document.getElementById('form-evento')
-
-let getValue = (elId) => document.getElementById(elId).value
+let getId = (elId) => document.getElementById(elId)
+let form = getId('form-evento')
+emailPattern =
+  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  emailPattern =
-    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
 
-  document.getElementById('await-result').classList.remove('hidden')
-  if (!getValue('email').match(emailPattern)) {
-    document.getElementById('invalid-email').classList.remove('hidden')
+  const Azienda = getId('azienda').value
+  const Nome = getId('nome').value
+  const Cognome = getId('cognome').value
+  const Email = getId('email').value
+  const Telefono = getId('telefono').value
+  const Data = getId('data').value
+  const Orario = getId('orario').value
+  const Pranzo = getId('pranzo').checked
+
+  getId('await-result').classList.remove('hidden')
+
+  if (!Email.match(emailPattern)) {
+    getId('invalid-email').classList.remove('hidden')
   } else {
-    document.getElementById('invalid-email').classList.add('hidden')
-    document.getElementById('server-error').classList.add('hidden')
+    getId('invalid-email').classList.add('hidden')
+    getId('server-error').classList.add('hidden')
 
-    let url = 'https://www.betaservice.it/betaservicerestws/api/generic/registrazioneevento'
+    const url = 'https://www.betaservice.it/betaservicerestws/api/generic/registrazioneevento'
     const options = {
       method: 'post',
       headers: {
@@ -24,25 +33,28 @@ form.addEventListener('submit', (e) => {
         IdCliente: 7,
         IdEvento: 1,
         Descrizione: 'Evento Infomat ottobre 2022',
-        Azienda: getValue('azienda'),
-        Nome: getValue('nome'),
-        Cognome: getValue('cognome'),
-        Email: getValue('email'),
-        Telefono: getValue('telefono'),
-        Data: getValue('data'),
-        Orario: getValue('orario'),
+        Azienda,
+        Nome,
+        Cognome,
+        Email,
+        Telefono,
+        Data,
+        Orario,
+        Pranzo,
       }),
     }
 
+    console.log(getId('pranzo').checked)
+
     fetch(url, options)
       .then(() => {
-        document.getElementById('await-result').classList.add('hidden')
-        document.getElementById('submit-button').disabled = true
-        document.getElementById('success-message').classList.remove('hidden')
+        getId('await-result').classList.add('hidden')
+        getId('submit-button').disabled = true
+        getId('success-message').classList.remove('hidden')
       })
       .catch(() => {
-        document.getElementById('await-result').classList.add('hidden')
-        document.getElementById('server-error').classList.remove('hidden')
+        getId('await-result').classList.add('hidden')
+        getId('server-error').classList.remove('hidden')
       })
   }
 })
